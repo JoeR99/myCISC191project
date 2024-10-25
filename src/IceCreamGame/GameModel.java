11d321d5fd460001026444aeefeff92d369980d4
Joe <Joe@DESKTOP-AR453BG>
2024-10-25 12:34:03 -0700

    Major classes that other classes aggregate to

--- src/IceCreamGame/GameModel.java
+++ src/IceCreamGame/GameModel.java
@@ -3,10 +3,50 @@ package IceCreamGame;
 
 public class GameModel
 {
+	// Models
+	private CashModel cashModel;
+	private ExperienceModel experienceModel;
+	private ScienceModel scienceModel;
 
 	public GameModel()
 	{
-		
+		this.cashModel = new CashModel();
+		this.experienceModel = new ExperienceModel();
+		this.scienceModel = new ScienceModel();
+	}
+	
+	// ===================================
+
+	// Getter Methods
+	
+	// ===================================
+	
+	public CashModel getCashModel()
+	{
+		return cashModel;
+	}
+	
+	public ExperienceModel getExperienceModel()
+	{
+		return experienceModel;
+	}
+	
+	public ScienceModel getScienceModel()
+	{
+		return scienceModel;
+	}
+	
+	// ===================================
+	
+	// Updates
+	
+	// ===================================
+	
+	public void updateGameState()
+	{
+		cashModel.updateCashModel();
+		experienceModel.updateExperienceModel();
+		scienceModel.updateScienceModel();
 	}
 	
 }
---
+++ src/IceCreamGame/GameView.java
@@ -0,0 +1,42 @@
+package IceCreamGame;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class GameView extends JFrame
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	private GameModel gameModel;
+
+	private final int WIDTH = 1280;
+	private final int HEIGHT = 720;
+	
+	public GameView(GameModel model)
+	{
+
+		this.gameModel = model;
+		
+		OuterPanel outerPanel = new OuterPanel(gameModel);
+
+		this.add(outerPanel);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Ice Cream Entrepreneur");
+		this.setSize(new Dimension(WIDTH, HEIGHT));
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		
+	}
+	
+	public void updateGameViewUI()
+	{
+		
+	}
+	
+}
